bd92d9a2f6c98dbde9c135bd3c7ee83eacc28e19
rockyuse <rockyuse@163.com>
2013-05-31 11:18:03 +0800

    map

--- js/map.js
+++ js/map.js
@@ -1,1 +1,338 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}(';!7(2v,$,2C){3 1s={}.2w;3 2i=7(1i,18){3 t,O,P;O={};R(t 14 1i){6(!1s.2k(1i,t))25;P=1i[t];O[t]=P};R(t 14 18){6(!1s.2k(18,t))25;P=18[t];O[t]=P};F O};3 f=(7(){7 f(17,k){5.17=17;5.2m(k);5.1T()};f.13.k={16:\'2A\',2q:2z,2u:2B,22:[\'2x\',\'2D\',\'2R\',\'2S\',\'2Q\',\'2P\',\'2V\'],Z:[\'U\',\'11\',\'12\',\'2T\'],1Y:\'2y\',1Z:\'\',b:{},26:1,27:\'2H\',U:\'2G\',11:\'2E\',12:\'2F\',1B:\'2M\',1C:2L,2g:20,1q:0,X:-10,2f:7(b,c){F c.2e},23:7(b,c){},24:7(b,c){},1z:M};f.13.2m=7(k){6(k==2l){k=2l};5.k=2i(5.k,k);F 5};f.13.2r=7(Y,9,a){3 d=n.1F(Y);6(!d.8.1g)d.8.1g="2K";d.8.9=9+"g";d.8.a=a+"g";d.8.1S="2J";3 v;6(1j.15=="1I"){d.1U="<1r:1G 8=\'1g : 1Q; 9: 1H; a: 1H; S: 1N; Q: 1N\' 2N=\'1b,1b\' 1x=\'1r\' u=\'1W"+Y+"\'><\\/1r:1G>";v=n.1F("1W"+Y)}E{d.1U="<T 1x=\'1X\'><\\/T>";v=d.2I("1X")[0]};3 m=V 1j(v,9,a);3 1v;6(1j.15=="2U"){m.2X.2W("2O","0 0 "+9+" "+a)}E{1v=d.21("T")[0]};m.1a=7(w,h,1R,N){N=!N;3 1y=w/9;3 1w=h/a;3 1d=1y<1w?1y:1w;3 A=B(a*1d);3 y=B(9*1d);6(1j.15=="1I"){3 1o=n.21("3o");R(3 i 14 1o){3 s=1o[i];6(s.8){6(!s.1n){3 1k=s.8.1P.3t("g");s.1n=B(1k[0]);s.1O=1k[1]};s.8.1P=s.1n*1d+"g"+s.1O}};3 G;6(y<A){G=y*1b/9}E{G=A*1b/a};G=B(G);v.8.9=G+"g";v.8.a=G+"g";6(N){v.8.Q=B((w-y)/2)+"g";v.8.S=B((h-A)/2)+"g"};1v.8.1S="3s"};6(N){y=w;A=h};d.8.9=y+"g";d.8.a=A+"g";m.3u(y,A);6(1R){d.8.1g="1Q";d.8.Q=B((w-y)/2)+"g";d.8.S=B((h-A)/2)+"g"}};m.3w=7(1D){m.1a(9*1D,a*1D)};m.1a(9,a);m.w=9;m.h=a;F m};f.13.1T=7(){3 4=5.k,2h=5.17,16=4.16,K=3v(16+\'3r\');3 b={};6(4.1Y==\'l\'){3 1V=4.1Z;$.3n({15:\'3m\',3y:1V,1K:M,3q:$.1L.1M?\'3p\':\'l\',3x:7(o){3 l;6($.1L.1M){l=V 3F(\'3E.3D\');l.1K=M;l.3G(o)}E{l=o};3 $l=$(l);$l.3C(\'b\').3z(7(i){3 $1u=$(5),1h=$1u.J(\'1h\');b[1h]={};R(3 i=0,1J=4.Z.2b;i<1J;i++){b[1h][4.Z[i]]=$1u.J(4.Z[i])}})}})}E{b=4.b};3 1m=7(e){3 C,x,1t=$(\'.z\').3A(),1p=$(\'.z\').36();6(e&&e.2n){C=e.2n;x=e.35}E{C=2t.34+n.1e.2o+n.2s.2o;x=2t.39+n.1e.2p+n.2s.2p};C=C-1t/2+4.1q<0?0:C-1t/2+4.1q;x=x-1p+4.X<0?x-4.X:x-1p+4.X;F[C,x]};3 H,1E;3 r=5.2r(2h.J(\'u\'),K.9,K.a),2a={D:\'#\'+4.U,2c:\'38\',1l:\'#\'+4.27,\'1l-9\':4.26,\'1l-37\':\'30\'};3 q={};R(3 j 14 K.28){3 p=b[j],I=\'#\'+(p&&4.22[p.U]||4.U),W=\'#\'+(p&&p.11||4.11),1f=\'#\'+(p&&p.12||4.12),29=\'#\'+(p&&p.1B||4.1B);q[j]={};q[j].I=I;q[j].W=W;q[j].1f=1f;3 c=r.2Z(K[\'28\'][j]);c.u=j;c.2e=K[\'2Y\'][j];c.J(2a);6(4.1z){4.1z[c.u]=c};6(b[j]&&b[j].33){c.J({D:29,2c:\'32\'})}E{c.J({D:I});c.31(7(e){6(5!=H){5.19({D:q[5.u].W},1c)};6(4.1C){3i(1E);6($(\'.z\').2b==0){$(n.1e).3h(\'<T 1x="z"></T\')};$(\'.z\').3g(4.2f(b,5));3 L=V 1m(e);$(\'.z\').2j({9:4.2g||\'2d\',a:4.3l||\'2d\',Q:L[0],S:L[1]}).3k()};4.23(b,5)});c.3j(7(){6(5!=H){5.19({D:q[5.u].I},1c)};6(4.1C){1E=3c(7(){$(\'.z\').3b()},20)}});c.3a(7(e){6(H){H.19({D:q[H.u].I},1c)};5.19({D:q[5.u].1f},1c);H=5;4.24(b,5)})};r.1a(4.2q,4.2u,M,M)};n.1e.3f=7(e){3 L=V 1m(e);$(\'.z\').2j({Q:L[0],S:L[1]})}};F f})();$.3e.f=7(1A){3 $5=$(5),o=$5.o();6(o.f){3d o.f};6(1A!==M){o.f=V f($5,1A)};F o.f}}(5,3B);',62,229,'|||var|opt|this|if|function|style|width|height|stateData|obj|wrapper||SVGMap|px|||state|options|xml|paper|document|data|thisStateData|stateColor||curr|key|id|nestedWrapper||mouseY|newWidth|stateTip|newHeight|parseInt|mouseX|fill|else|return|newSize|current|initColor|attr|mapConfig|_offsetXY|false|clipping|out|val|left|for|top|div|stateInitColor|new|hoverColor|stateTipY|container|stateDataAttr||stateHoverColor|stateSelectedColor|prototype|in|type|mapName|dom|obj2|animate|changeSize|1000|250|scale|body|selectedColor|position|stateName|obj1|Raphael|mod|stroke|offsetXY|_fontSize|txt|tipHeight|stateTipX|rvml|__hasProp|tipWidth|node|vmlDiv|ratioH|class|ratioW|external|opts|stateDisabledColor|showTip|amount|reTimer|getElementById|group|1000px|VML|len|async|browser|msie|0px|_font|font|absolute|center|overflow|render|innerHTML|mapSettings|vmlgroup_|svggroup|stateDataType|stateSettingsXmlPath|100|getElementsByTagName|stateColorList|hoverCallback|clickCallback|continue|strokeWidth|strokeColor|shapes|disabledColor|attributes|length|cursor|auto|name|stateTipHtml|stateTipWidth|_self|mergeObjects|css|call|null|setOptions|pageX|scrollLeft|scrollTop|mapWidth|scaleRaphael|documentElement|event|mapHeight|win|hasOwnProperty|2770B5|json|500|china|400|undefined|429DD4|feb41c|E32F02|AAD5FF|F9FCFE|getElementsByClassName|hidden|relative|true|eeeeee|coordsize|viewBox|C6E1F4|70B3DD|5AABDA|1C8DFF|baifenbi|SVG|EDF2F6|setAttribute|canvas|names|path|round|hover|default|diabled|clientX|pageY|outerHeight|linejoin|pointer|clientY|mouseup|remove|setTimeout|delete|fn|onmousemove|html|append|clearTimeout|mouseout|show|stateTipHeight|GET|ajax|textpath|text|dataType|MapConfig|visible|split|setSize|eval|scaleAll|success|url|each|outerWidth|jQuery|find|XMLDOM|Microsoft|ActiveXObject|loadXML'.split('|'),0,{}))
+/**
+ * AvatarUI-地图
+ * @version v2.0.1
+ * @author  Rocky(296456018@qq.com)
+ * @date    2013-05-28
+ */
+
+;!function(win, $, undefined){
+    var __hasProp = {}.hasOwnProperty;
+
+    var mergeObjects = function (obj1, obj2) {
+        var key, out, val;
+        out = {};
+        for (key in obj1) {
+            if (!__hasProp.call(obj1, key)) continue;
+            val = obj1[key];
+            out[key] = val;
+        }
+        for (key in obj2) {
+            if (!__hasProp.call(obj2, key)) continue;
+            val = obj2[key];
+            out[key] = val;
+        }
+        return out;
+    };
+
+    var SVGMap = (function(){
+        function SVGMap(dom, options){
+            this.dom = dom;
+            this.setOptions(options);
+            this.render();
+        }
+
+        SVGMap.prototype.options = {
+            mapName: 'china',
+            mapWidth: 500,
+            mapHeight: 400,
+            stateColorList: ['2770B5', '429DD4', '5AABDA', '1C8DFF', '70B3DD', 'C6E1F4', 'EDF2F6'],
+            stateDataAttr: ['stateInitColor', 'stateHoverColor', 'stateSelectedColor', 'baifenbi'],
+            stateDataType: 'json',
+            stateSettingsXmlPath: '',
+            stateData: {},
+            
+            strokeWidth: 1,
+            strokeColor: 'F9FCFE',
+
+            stateInitColor: 'AAD5FF',
+            stateHoverColor: 'feb41c',
+            stateSelectedColor: 'E32F02',
+            stateDisabledColor: 'eeeeee',
+
+            showTip: true,
+            stateTipWidth: 100,
+            //stateTipHeight: 50,
+            stateTipX: 0,
+            stateTipY: -10,
+            stateTipHtml: function(stateData, obj){
+                return obj.name;
+            },
+
+            hoverCallback: function(stateData, obj){},
+            clickCallback: function(stateData, obj){},
+            external: false
+        };
+        SVGMap.prototype.setOptions = function (options) {
+            if (options == null) {
+                options = null;
+            }
+            this.options = mergeObjects(this.options, options);
+            return this;
+        };
+
+        SVGMap.prototype.scaleRaphael = function(container, width, height) {
+                var wrapper = document.getElementById(container);
+                if (!wrapper.style.position) wrapper.style.position = "relative";
+                wrapper.style.width = width + "px";
+                wrapper.style.height = height + "px";
+                wrapper.style.overflow = "hidden";
+                var nestedWrapper;
+                if (Raphael.type == "VML") {
+                    wrapper.innerHTML = "<rvml:group style='position : absolute; width: 1000px; height: 1000px; top: 0px; left: 0px' coordsize='1000,1000' class='rvml' id='vmlgroup_" + container + "'><\/rvml:group>";
+                    nestedWrapper = document.getElementById("vmlgroup_" + container);
+                } else {
+                    wrapper.innerHTML = "<div class='svggroup'><\/div>";
+                    nestedWrapper = wrapper.getElementsByClassName("svggroup")[0];
+                }
+                var paper = new Raphael(nestedWrapper, width, height);
+                var vmlDiv;
+                if (Raphael.type == "SVG") {
+                    paper.canvas.setAttribute("viewBox", "0 0 " + width + " " + height);
+                } else {
+                    vmlDiv = wrapper.getElementsByTagName("div")[0];
+                }
+                paper.changeSize = function (w, h, center, clipping) {
+                    clipping = !clipping;
+                    var ratioW = w / width;
+                    var ratioH = h / height;
+                    var scale = ratioW < ratioH ? ratioW : ratioH;
+                    var newHeight = parseInt(height * scale);
+                    var newWidth = parseInt(width * scale);
+                    if (Raphael.type == "VML") {
+                        var txt = document.getElementsByTagName("textpath");
+                        for (var i in txt) {
+                            var curr = txt[i];
+                            if (curr.style) {
+                                if (!curr._fontSize) {
+                                    var mod = curr.style.font.split("px");
+                                    curr._fontSize = parseInt(mod[0]);
+                                    curr._font = mod[1];
+                                }
+                                curr.style.font = curr._fontSize * scale + "px" + curr._font;
+                            }
+                        }
+                        var newSize;
+                        if (newWidth < newHeight) {
+                            newSize = newWidth * 1000 / width;
+                        } else {
+                            newSize = newHeight * 1000 / height;
+                        }
+                        newSize = parseInt(newSize);
+                        nestedWrapper.style.width = newSize + "px";
+                        nestedWrapper.style.height = newSize + "px";
+                        if (clipping) {
+                            nestedWrapper.style.left = parseInt((w - newWidth) / 2) + "px";
+                            nestedWrapper.style.top = parseInt((h - newHeight) / 2) + "px";
+                        }
+                        vmlDiv.style.overflow = "visible";
+                    }
+                    if (clipping) {
+                        newWidth = w;
+                        newHeight = h;
+                    }
+                    wrapper.style.width = newWidth + "px";
+                    wrapper.style.height = newHeight + "px";
+                    paper.setSize(newWidth, newHeight);
+                    if (center) {
+                        wrapper.style.position = "absolute";
+                        wrapper.style.left = parseInt((w - newWidth) / 2) + "px";
+                        wrapper.style.top = parseInt((h - newHeight) / 2) + "px";
+                    }
+                };
+                paper.scaleAll = function (amount) {
+                    paper.changeSize(width * amount, height * amount);
+                };
+                paper.changeSize(width, height);
+                paper.w = width;
+                paper.h = height;
+                return paper;
+        }
+
+        SVGMap.prototype.render = function(){
+            var opt = this.options, 
+                _self = this.dom,
+                mapName = opt.mapName,
+                mapConfig = eval(mapName + 'MapConfig');
+
+            var stateData = {};
+
+            if(opt.stateDataType == 'xml'){
+                console.log(123)
+                var mapSettings = opt.stateSettingsXmlPath;
+                $.ajax({
+                    type: 'GET',
+                    url: mapSettings,
+                    async: false,
+                    dataType: $.browser.msie ? 'text' : 'xml',
+                    success: function (data) {
+                        var xml;
+                        if ($.browser.msie) {
+                            xml = new ActiveXObject('Microsoft.XMLDOM');
+                            xml.async = false;
+                            xml.loadXML(data);
+                        } else {
+                            xml = data;
+                        }
+                        var $xml = $(xml);
+                        $xml.find('stateData').each(function(i) {
+                            var $node = $(this),
+                                stateName = $node.attr('stateName');
+                            
+                            stateData[stateName] = {};
+                            // var attrs = $node[0].attributes;
+                            // alert(attrs);
+                            // for(var i in attrs){
+                            //     stateData[stateName][attrs[i].name] = attrs[i].value;
+                            // }
+                            for(var i = 0, len = opt.stateDataAttr.length; i < len; i++){
+                                stateData[stateName][opt.stateDataAttr[i]] = $node.attr(opt.stateDataAttr[i]);
+                            }
+                        });
+                    }
+                });
+            }else{
+                stateData = opt.stateData;
+            }
+            
+            var offsetXY = function(e){
+                var mouseX, 
+                    mouseY,
+                    tipWidth = $('.stateTip').outerWidth(),
+                    tipHeight = $('.stateTip').outerHeight();
+                if(e && e.pageX){
+                    mouseX = e.pageX;
+                    mouseY = e.pageY;
+                }else{
+                    mouseX = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                    mouseY = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+                }
+                mouseX = mouseX - tipWidth/2 + opt.stateTipX < 0 ? 0 : mouseX - tipWidth/2 + opt.stateTipX;
+                mouseY = mouseY - tipHeight + opt.stateTipY < 0 ? mouseY - opt.stateTipY : mouseY - tipHeight + opt.stateTipY;
+                return [mouseX, mouseY];
+            };
+
+            var current, reTimer;
+
+            var r = this.scaleRaphael(_self.attr('id'), mapConfig.width, mapConfig.height),
+                attributes = {
+                    fill: '#' + opt.stateInitColor,
+                    cursor: 'pointer',
+                    stroke: '#' + opt.strokeColor,
+                    'stroke-width': opt.strokeWidth,
+                    'stroke-linejoin': 'round'
+                };
+
+            var stateColor = {};
+           
+            for (var state in mapConfig.shapes) {
+                var thisStateData = stateData[state],
+                    initColor = '#' + (thisStateData && opt.stateColorList[thisStateData.stateInitColor] || opt.stateInitColor),
+                    hoverColor = '#' + (thisStateData && thisStateData.stateHoverColor || opt.stateHoverColor),
+                    selectedColor = '#' + (thisStateData && thisStateData.stateSelectedColor || opt.stateSelectedColor),
+                    disabledColor = '#' + (thisStateData && thisStateData.stateDisabledColor || opt.stateDisabledColor);
+                
+                stateColor[state] = {};
+
+                stateColor[state].initColor = initColor;
+                stateColor[state].hoverColor = hoverColor;
+                stateColor[state].selectedColor = selectedColor;
+
+                var obj = r.path(mapConfig['shapes'][state]);
+                obj.id = state;
+                obj.name = mapConfig['names'][state];
+                obj.attr(attributes);
+
+                if(opt.external){
+                    opt.external[obj.id] = obj;
+                }
+
+                if(stateData[state] && stateData[state].diabled){
+                    obj.attr({
+                        fill: disabledColor,
+                        cursor: 'default'
+                    });
+                }else{
+                    obj.attr({
+                        fill: initColor
+                    });
+                    
+                    obj.hover(function(e){
+                        if (this != current){
+                            this.animate({
+                                fill: stateColor[this.id].hoverColor
+                            }, 250);
+                        }
+                        if(opt.showTip){
+                            clearTimeout(reTimer);
+                            if ($('.stateTip').length == 0) {
+                                $(document.body).append('<div class="stateTip"></div');
+                            }
+                            $('.stateTip').html(opt.stateTipHtml(stateData, this));
+                            var _offsetXY = new offsetXY(e);
+
+                            $('.stateTip').css({
+                                width: opt.stateTipWidth || 'auto',
+                                height: opt.stateTipHeight || 'auto',
+                                left: _offsetXY[0],
+                                top: _offsetXY[1]
+                            }).show();
+                        }
+
+                        opt.hoverCallback(stateData, this);
+                    });
+
+                    obj.mouseout(function(){
+                        if (this != current) {
+                            this.animate({
+                                fill: stateColor[this.id].initColor
+                            }, 250);
+                        }
+                        // $('.stateTip').hide();
+                        if(opt.showTip){
+                            reTimer = setTimeout(function(){
+                                $('.stateTip').remove();
+                            }, 100);
+                        }
+                    });
+
+                    obj.mouseup(function(e) {
+                        if(current) {
+                            current.animate({
+                                fill: stateColor[current.id].initColor
+                            }, 250);
+                        }
+
+                        this.animate({
+                            fill: stateColor[this.id].selectedColor
+                        }, 250);
+
+                        current = this;
+                        opt.clickCallback(stateData, this);
+                    });
+                }
+                r.changeSize(opt.mapWidth, opt.mapHeight, false, false);
+            }
+            document.body.onmousemove = function(e){
+                var _offsetXY = new offsetXY(e);
+                $('.stateTip').css({
+                    left: _offsetXY[0],
+                    top: _offsetXY[1]
+                });
+            };
+        }
+        return SVGMap;
+    })();
+
+    $.fn.SVGMap = function (opts) {
+        var $this = $(this),
+            data = $this.data();
+
+        if (data.SVGMap) {
+            delete data.SVGMap;
+        }
+        if (opts !== false) {
+            data.SVGMap = new SVGMap($this, opts);
+        }
+        return data.SVGMap;
+    };
+}(this, jQuery);
